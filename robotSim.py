512ce02a496dce6e0082bf2a87e48007a9002b61
Kazuho Kobayashi <kazuho.kobayashi.ynu@gmail.com>
2023-07-12 05:28:25 +0200

    update

--- robotSim.py
+++ robotSim.py
@@ -1,5 +1,6 @@
 import pygame
 import math
+import os
 
 class Robot:
     def __init__(self, startpos, robotimg, width, follow=None):
@@ -8,7 +9,9 @@ class Robot:
 
         self.x, self.y = startpos
         self.theta = 0
+        self.trail_set = []
         self.w = width
+        self.a = 20
 
         self.u = 30         # linear velocity[pix/sec]
         self.w = 0          # angular velocity[rad/sec]
@@ -18,11 +21,35 @@ class Robot:
         self.rotated = self.img
         self.rect = self.rotated.get_rect(center=(self.x, self.y))
 
-    def move(self):
-        pass
+    def move(self, event = None):
+        self.x += (self.u*math.cos(self.theta) - self.a*math.sin(self.theta)*self.w) * dt
+        self.y += (self.u*math.sin(self.theta) - self.a*math.cos(self.theta)*self.w) * dt
+        self.theta += self.w * dt
 
+        self.rotated = pygame.transform.rotozoom(self.img,
+                                                 math.degrees(-self.theta), 1)
+        self.rect = self.rotated.get_rect(center=(self.x, self.y))
+
+        if self.leader == True:
+            if event is not None:
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_KP4:
+                        self.u += 0.001 * self.m2p
+                    elif event.key == pygame.K_KP1:
+                        self.u -= 0.001 * self.m2p
+                    elif event.key == pygame.K_KP6:
+                        self.w += 0.001 * self.m2p
+                    elif event.key == pygame.K_KP3:
+                        self.w -= 0.001 * self.m2p
+        else:
+            self.following()
+    
     def following(self):
-        pass
+        target = self.follow.trail_set[0]
+        delta_x = target[0] - self.x
+        delta_y = target[1] - self.y
+        self.u = delta_x*math.cos(self.theta) + delta_y*math.sin(self.theta)
+        self.w = (-1/self.a)*math.sin(self.theta)*delta_x + (1/self.a)*math.cos(self.theta)*delta_y
 
     def dist(self, point1, point2):
         (x1, y1) = point1
@@ -38,12 +65,18 @@ class Robot:
         distance = (px + py) ** (0.5)
         return distance
 
-    def draw(self):
+    def draw(self, map):
         map.blit(self.rotated, self.rect)
         pass
 
-    def trail(self):
-        pass
+    def trail(self, pos, map, color):
+        for i in range(0, len(self.trail_set)-1):
+            pygame.draw.line(map, color, 
+                             (self.trail_set[i][0], self.trail_set[i][1]),
+                             (self.trail_set[i+1][0], self.trail_set[i+1][1]))
+            if self.trail_set.__sizeof__() > 2000:
+                self.trail_set.pop(0)
+            self.trail_set.append(pos)
 
 class Envir:
     def __init__(self, dimentions):
@@ -69,14 +102,22 @@ class Envir:
     def robot_frame(self):
         pass
 
-# initialization
+# initialization area
+skins = [r'fig/robot_green.png',
+         r'fig/robot_red.png'
+         r'fig/robot_blue.png'
+         r'fig/robot_orange.png'
+         r'fig/robot_purple.png']
+
 pygame.init()
 running = True
 iterations = 0
+dt = 0
+lasttime = pygame.time.get_ticks()
 start = (200, 200)
 dims = (600, 1200)
 environment = Envir(dims)
-robot = Robot(start, r'',
+robot = Robot(start, r'fig/robot_green.png',
               width = 80,
               follow = None)
 
@@ -87,5 +128,8 @@ while running:
             running = False
 
     environment.map.fill(environment.black)
+    robot.draw(environment.map)
     pygame.display.update()
+    dt = (pygame.time.get_ticks() - lasttime) / 1000    # seconds
+    lasttime = pygame.time.get_ticks()
     iterations+=1
